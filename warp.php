d6eaf2b1f2fd61644be82d5a9f91c97dacadbf68
Ilker Binzet <ilker.binzet@gmail.com>
2020-07-11 15:30:45 +0300

    Add files via upload

---
+++ warp.php
@@ -0,0 +1,25 @@
+<?php
+system("clear");
+$id = "3c7370bc-b2c1-4807-8d6e-df00b2518334";
+echo "\n";
+for ($i=1; $i <= 300; $i++) {
+	sleep(18); // Recommend 18 second
+	$api = curl($id);
+	if ($api == "success") {
+		echo "$i.Success Send 1GB\n";
+	} else {
+		echo "$i.Failed Send 1GB\n";
+	}
+}
+
+function curl($id) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.nabil.my.id/warp");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, 'id='.$id.'');
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
+?>
